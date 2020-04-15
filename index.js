4a1ad0cc72b2aa14567f59cbe754b954ea2ff5a2
felippemozer <45230333+felippemozer@users.noreply.github.com>
2020-04-15 15:43:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://felippe:felippe@cluster0-shard-00-00-ahjri.mongodb.net:27017,cluster0-shard-00-01-ahjri.mongodb.net:27017,cluster0-shard-00-02-ahjri.mongodb.net:27017/imc?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+app.use(express.json());
+app.use(routes);
+
+var porta = process.env.PORT || 8080
+
+app.listen(porta);
